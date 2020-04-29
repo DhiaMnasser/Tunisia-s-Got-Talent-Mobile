c4eabcb81c0a30deafe0d372511e79ba43cb8c9d
gthadien <gthadrien111@gmail.com>
2020-04-29 03:10:51 +0100

    version_de_base_2

---
+++ src/com/mycompany/myapp/services/achraf/Statics.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp.services.achraf;
+
+/**
+ *
+ * @author gth
+ */
+public class Statics {
+    public static final String BASE_URL="http://localhost:81/tgtOf/web/app_dev.php/api";
+}
